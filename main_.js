d0c0bb46dcaaae1900cc80bc10e4f9b761b236db
Twisha-M-Gupta <90875017+Twisha-M-Gupta@users.noreply.github.com>
2022-10-17 16:30:46 -0400

    Add files via upload

---
+++ main_.js
@@ -0,0 +1,22 @@
+function gameOpen() {
+    window.location = "normalGame.html";
+}
+
+function preload() {
+    world_start = loadSound("world_start.wav");
+    mario_die = loadSound("mariodie.wav");
+    mario_gameover = loadSound("gameover.wav");
+    reward = loadSound("coin.wav ");
+    setSprites();
+    MarioAnimation();
+}
+
+function setup() {
+    canvas = createCanvas(1240, 336);
+    instializeInSetup(mario);
+
+}
+
+function draw() {
+    game();
+}
